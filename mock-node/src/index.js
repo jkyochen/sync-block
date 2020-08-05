8efcc4b350c3277cb625ee7106b2d523ae63edce
Lanlyhs <ziqiang.chen@outlook.com>
2020-08-05 14:13:27 +0800

    update: panic when don't have .env

--- mock-node/src/index.js
+++ mock-node/src/index.js
@@ -1,7 +1,11 @@
-require("dotenv").config();
+const result = require("dotenv").config()
+
+if (result.error) {
+    throw result.error
+}
 
 const app = require('./app')
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.port;
 
 app.listen(PORT, function () {
     console.log(`Listening on port ${PORT}...`);
